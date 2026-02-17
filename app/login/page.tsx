867aa5b4ae444d3dcdffb7bcb339ff48b34c1bdf
dharshan-24 <dharshanlak2005@gmail.com>
2026-02-17 13:18:32 +0530

    update redirect url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/dashboard",
+        redirectTo: "https://smart-bookmark-app-lemon-eight.vercel.app/login",
       },
     });
   };
